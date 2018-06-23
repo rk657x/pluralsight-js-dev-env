70edce82c0b59b748f857ef97634ba9534125605
Ryan Miller <rk657x@gmail.com>
2018-06-22 17:40:34 -0700

    add linting to project

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,6 +3,7 @@ import path  from 'path';
 import open  from 'open';
 import webpack from 'webpack';
 import config from '../buildScripts/webpack.config.dev';
+/*eslint-disable no-console */
 
 const port = 3000;
 const app = express();
